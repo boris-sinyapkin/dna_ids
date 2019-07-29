805270110a4bc28c3677e301897d7cab23982500
Boris <griboris01@gmail.com>
2019-07-30 01:21:42 +0300

    Small fixes

--- run.py
+++ run.py
@@ -1,23 +1,20 @@
-
 import csv
 import ids
 
-from pathlib import Path
+from pathlib     import Path
+from kdd_dataset import KDD_Dataset
 
-def run(dataset_path : Path, codetable_path : Path, normal_path : Path):
 
-    # Prepare dataset
-    dataset = ids.kdd_dataset(dataset_path)
+def run(dataset_path: Path, codetable_path: Path, normal_path: Path):
 
-    # Set codetable
-    dataset.set_codetable(codetable_path)
+    train_dataset = KDD_Dataset(dataset_path, codetable_path)
 
     normal_act = []
 
     # Prepare file with normal activities (if not exist)
     if normal_path.exists() == False:
         # Select normal activities from dataset
-        normal_act = dataset.findall("normal",
+        normal_act = train_dataset.findall("normal",
                                      lambda val, seq_rec: val == seq_rec.name)
         # Put it in FASTA fromat file
         ids.SeqIO.write(normal_act, "normal.faa", "fasta")
@@ -31,14 +28,9 @@ def run(dataset_path : Path, codetable_path : Path, normal_path : Path):
     aligner = ids.Align.PairwiseAligner()
 
     # Match/mismatch scores of 5/-4 and gap penalties (open/extend) of 2/0.5):
-    aligner.match            =  5.0
-    aligner.mismatch         = -4.0
-    aligner.open_gap_score   = -2.0
+    aligner.match = 5.0
+    aligner.mismatch = -4.0
+    aligner.open_gap_score = -2.0
     aligner.extend_gap_score = -0.5
 
-    ideal_seq = ids.get_normal_sequence(aligner, normal_act)
-        
-
-
-        
-
+    ideal_seq = ids.get_best_signature(aligner, normal_act)
